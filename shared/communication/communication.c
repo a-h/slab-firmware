c4345e6b122e1adbc595d7a676c4db2c0b663e62
Edward Hesketh <headblockhead@gmail.com>
2024-11-03 17:46:54 +0000

    feat: split keyboard working!

--- shared/communication/communication.c
+++ shared/communication/communication.c
@@ -4,6 +4,7 @@
 #include <pico/stdlib.h>
 #include <squirrel_consumer.h>
 #include <squirrel_keyboard.h>
+#include <squirrel_split.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -13,11 +14,11 @@ uint8_t address = 0b00010111;
 i2c_inst_t *slave_i2c_inst;
 i2c_inst_t *master_i2c_inst;
 
-uint8_t keyboard_recv_buf[6];
-uint8_t keyboard_recv_buf_index = 0;
-uint8_t keyboard_modifier_buf[1];
+uint8_t squirrel_recv_buf[9];
+uint8_t squirrel_recv_buf_index = 0;
 
-uint8_t com_type = 0; // If 0, we need to read for the new type of data.
+uint8_t com_type =
+    0; // If 0, we need to read a byte to determine the new type of data.
 
 static void i2c_slave_handler(i2c_inst_t *i2c, i2c_slave_event_t event) {
   switch (event) {
@@ -27,13 +28,9 @@ static void i2c_slave_handler(i2c_inst_t *i2c, i2c_slave_event_t event) {
       break;
     };
     switch (com_type) {
-    case COM_TYPE_KEYBOARD_KEYCODE:
-      keyboard_recv_buf[keyboard_recv_buf_index] = i2c_read_byte_raw(i2c);
-      keyboard_recv_buf_index++;
-      break;
-    case COM_TYPE_KEYBOARD_MODIFIER:
-      break;
-    case COM_TYPE_CONSUMER:
+    case COM_TYPE_SQUIRREL:
+      squirrel_recv_buf[squirrel_recv_buf_index] = i2c_read_byte_raw(i2c);
+      squirrel_recv_buf_index++;
       break;
     default:
       printf("U");
@@ -43,8 +40,10 @@ static void i2c_slave_handler(i2c_inst_t *i2c, i2c_slave_event_t event) {
   case I2C_SLAVE_REQUEST: // master is requesting data
     break;
   case I2C_SLAVE_FINISH: // master has signalled Stop / Restart
-    keyboard_recv_buf_index = 0;
+    squirrel_recv_buf_index = 0;
     com_type = 0;
+    process_packet(&squirrel_recv_buf);
+    memset(squirrel_recv_buf, 0, 9);
     break;
   default: // other/unknown event
     break;
@@ -59,24 +58,14 @@ void communication_init(i2c_inst_t *master_i2c, i2c_inst_t *slave_i2c,
   i2c_slave_init(slave_i2c_inst, address, &i2c_slave_handler);
 };
 
-uint8_t send_kb_kc_buf[7] = {COM_TYPE_KEYBOARD_KEYCODE};
-uint8_t send_kb_mod_buf[2] = {COM_TYPE_KEYBOARD_MODIFIER};
-uint8_t send_consumer_buf[3] = {COM_TYPE_CONSUMER};
+uint8_t squirrel_send_buf[9] = {0};
+uint8_t i2c_send_buf[10] = {COM_TYPE_SQUIRREL};
 
 void communication_task(mutex_t *i2c_mutex) {
-  memset(send_kb_kc_buf + 1, 0, 6);
-  uint8_t active_keycodes[6] = {0};
-  keyboard_get_keycodes(&active_keycodes);
-  memcpy(send_kb_kc_buf + 1, active_keycodes, 6);
-
-  /*send_kb_mod_buf[1] = keyboard_get_modifiers();*/
-
-  /*send_consumer_buf[1] = consumer_code & 0xFF;*/
-  /*send_consumer_buf[2] = consumer_code >> 8;*/
+  get_packet(&squirrel_send_buf);
+  memcpy(i2c_send_buf + 1, squirrel_send_buf, 9);
 
   /*mutex_enter_blocking(i2c_mutex);*/
-  i2c_write_blocking(master_i2c_inst, address, send_kb_kc_buf, 7, false);
-  /*i2c_write_blocking(master_i2c_inst, address, send_kb_mod_buf, 2, false);*/
-  /*i2c_write_blocking(master_i2c_inst, address, send_consumer_buf, 3, false);*/
+  i2c_write_blocking(master_i2c_inst, address, i2c_send_buf, 10, false);
   /*mutex_exit(i2c_mutex);*/
 }
--- shared/communication/communication.h
+++ shared/communication/communication.h
@@ -6,9 +6,7 @@
 #include <pico/stdlib.h>
 
 enum com_type {
-  COM_TYPE_KEYBOARD_KEYCODE = 1,
-  COM_TYPE_KEYBOARD_MODIFIER,
-  COM_TYPE_CONSUMER,
+  COM_TYPE_SQUIRREL = 1,
 };
 
 void communication_init(i2c_inst_t *master_i2c_inst, i2c_inst_t *slave_i2c_inst,
